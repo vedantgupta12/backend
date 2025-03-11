b6654d41afd13ac83967050781f024628f5ddce2
Vedant Gupta <118821135+vedantgupta12@users.noreply.github.com>
2025-03-11 10:25:12 +0530

    Update database.js

--- fileupload/config/database.js
+++ fileupload/config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 exports.connect=()=>{
-    mongoose.connect("mongodb+srv://vedant12:ved2003%40@cluster2.zrm9sc9.mongodb.net/fileupload")
+    mongoose.connect("")
     .then(()=>{
         console.log("Database connected successfully")
     })
